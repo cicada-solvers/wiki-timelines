1a70594da49ab815f05b9485c833d2a3b5bfc7ba
tweqx <romla@sfr.fr>
2022-03-29 16:30:46 +0200

    Remove unused & outdated code

--- timeline.js
+++ timeline.js
@@ -318,25 +318,7 @@ class Timeline {
         group: group_id,
 
         content: d['title'],
-        information: d['data'],
-
-        more_information: (event, content) => {
-          const text = txt => document.createTextNode(txt);
-          const newline = document.createElement('br');
-
-          let info = event.data;
-
-          if (info.distinguished)
-            content.appendChild(text(`Author: ${info.author} (as a moderator)`));
-          else
-            content.appendChild(text(`Author: ${info.author}`));
-          content.appendChild(newline);
-
-          let link = document.createElement('a');
-          link.href = info.url;
-          link.innerText = "View post";
-          content.appendChild(link);
-        }
+        information: d['data']
       };
 
       // Category?
